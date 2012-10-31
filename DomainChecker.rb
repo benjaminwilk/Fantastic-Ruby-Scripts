c6703b548d171c1e41181e9fbffafe53cedc535b
Ben Wilk <benjaminawilk@gmail.com>
2012-10-31 11:53:56 -0400

    working on making it more universal again

--- DomainChecker.rb
+++ DomainChecker.rb
@@ -8,7 +8,7 @@ Version: 1.0
 full_domain_path = `locate 'conf.d/vhost_' | grep -v 000_defaults.conf`.chomp.split(' ')
 #domain_name = `ls /etc/httpd/conf.d/vhost_* | awk -F'_' '{print $2}'| awk -F'.conf' '{print $1}'`.chomp.split(' ')
 full_domain_path.each_index do |x|
-  puts domain_name = `ls #{full_domain_path[x]} | awk -F'_' '{print $2}'| awk -F'.conf' '{print $1}'`.chomp.split(' ')
+  domain_name = `ls #{full_domain_path[x]} | awk -F'_' '{print $2}'| awk -F'.conf' '{print $1}'`.chomp.split(' ')
 end
 #ip_address = `grep '<VirtualHost*' #{full_domain_path}`
 #dug_domain = `dig a #{domain_name} @8.8.8.8`
