bdbba4ae761a66f05f362669a1dba0d84f0631e5
Ben Wilk <benjaminawilk@gmail.com>
2012-11-01 09:13:09 -0400

    Cleaned up the code, removed redundancy

--- DomainChecker.rb
+++ DomainChecker.rb
@@ -4,17 +4,17 @@ Last Revision: Oct 31, 2012
 Version: 1.0
 =end
 
-#full_domain_path = `ls /etc/httpd/conf.d/vhost_*`.chomp.split(' ')
-full_domain_path = `locate 'conf.d/vhost_' | grep -v 000_defaults.conf`.chomp.split(' ')
-#domain_name = `ls /etc/httpd/conf.d/vhost_* | grep -v 000 | awk -F'_' '{print $2}'| awk -F'.conf' '{print $1}'`.chomp.split(' ')
-domain_name = `locate /etc/httpd/conf.d/vhost_ | grep -v 000_defaults | awk -F'/' '{print $5}' | awk -F'vhost_' '{print $2}' | awk -F'.conf' '{print $1}'`.strip.split(' ')
+  full_domain_path = `locate 'conf.d/vhost_' | grep -v 000_defaults.conf`.chomp.split(' ')
+  full_domain_path.each_index do |x|
+     domain_name[x] = `echo '#{full_domain_path[x]}' | awk -F'/' '{print $5}' | awk -F'vhost_' '{print $2}' | awk -F'.conf' '{print $1}'`
+  end
 
-puts "\n%s %40s %43s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
-domain_name.each_index { |x|
-  padding = 50
 
-  print "#{domain_name[x]}"
-  padding = padding.to_i - domain_name[x].length
-  print "%0#{padding}s" %[`grep '<VirtualHost .*:80>' #{full_domain_path[x]} | awk -F'<VirtualHost' '{print $2}'|awk -F':' '{print $1}'`.strip.to_s]
-  puts "%40s" %[`dig #{domain_name[x]} @8.8.8.8 +noall +short`.strip]
-}
+  puts "\n%s %40s %43s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
+  domain_name.each_index { |x|
+    padding = 50
+    print "#{domain_name[x]}"
+    padding = padding.to_i - domain_name[x].length
+    print "%0#{padding}s" %[`grep '<VirtualHost .*:80>' #{full_domain_path[x]} | awk -F'<VirtualHost' '{print $2}'|awk -F':' '{print $1}'`.strip.to_s]
+    puts "%40s" %[`dig #{domain_name[x]} @8.8.8.8 +noall +short`.strip]
+  }
