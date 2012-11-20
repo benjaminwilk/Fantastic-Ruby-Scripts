42795337440f429398e1f99cd9d3235400491f6a
Ben Wilk <benjaminawilk@gmail.com>
2012-11-20 11:36:51 -0500

    Another re-write

--- DomainChecker.rb
+++ DomainChecker.rb
@@ -5,56 +5,55 @@ Last Revision: Nov 14, 2012
 Version: 2.0
 =end
 
-class Domain_check
+class DomainCheck
   def vhost_grab
-    full_domain_path = []
-    Dir["/etc/httpd/conf.d/vhost_*"].each {|x|
-      if x.include? "000"
-        print ""
-      else
-        full_domain_path.push("#{x}")
-      end
-    }
-    return full_domain_path.sort!
-  end
-
-  def zero_remover
-    vhost =[]
-    vhosts = vhost_grab  
-    value = "000"
-    vhosts.sort!.each_index do |x|
-    if vhosts[x].include? value 
-      print ''
+    vhosts = []
+    Dir["/etc/httpd/conf.d/vhost_*"].each { |d|
+    if d.include? "000"
+      print ""
     else
-       vhost[x] = vhosts[x]
-     end 
+      vhosts.push(d)
     end
-  return vhost.compact
+   }
+  return vhosts
   end
 
-  def before_after
-    stripper = zero_remover
-   stripper.each_index do |x|
-       stripper[x] = stripper[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
+  def stripper
+    vhosts = vhost_grab
+    value = []
+
+    vhosts.each_index do |x|
+      value[x] = open(vhosts[x]).grep(/<VirtualHost .*:80>/).to_s
+      value[x].gsub!(/:80>/,"").gsub!(/<VirtualHost/, "")
     end
-    final_vhost = stripper
-   return final_vhost
+  return value 
   end
 
-  def vhost_display
-    final_vhost = before_after
+  def shortened
+    vhosts = vhost_grab
+    stripped_vhosts = []
+    vhosts.each_index do |x|
+      stripped_vhosts[x] = vhosts[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
+    end
+  return stripped_vhosts
+  end
+
+  def domain_display
+    stripped_vhosts = shortened
+    value = stripper
+    vhosts = vhost_grab
     puts "\n%s %40s %43s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
-    final_vhost.each_index { |x|
-      padding = 50
-      print final_vhost[x].strip
-      padding = padding.to_i - final_vhost[x].length
-      print "%0#{padding}s" %[`grep '<VirtualHost .*:80>' #{vhost_grab[x]} | awk -F'<VirtualHost' '{print $2}'|awk -F':' '{print $1}'`.strip.to_s]
-      puts "%40s" %[`dig #{final_vhost[x].strip} +short`]
-    }
+    value.each_index do |x|
+
+      padding = 80
+      padding = padding.to_i - vhosts[x].length
+
+      print "%s %#{padding}s %35s" %[stripped_vhosts[x], value[x].strip, `dig #{stripped_vhosts[x].strip} +short`.strip]
+      puts "\n"
+    end
   end
+
 end
 
-d1 = Domain_check.new
-d1.zero_remover
-d1.vhost_display
-#d1.stripper
+d1 = DomainCheck.new
+d1.domain_display
