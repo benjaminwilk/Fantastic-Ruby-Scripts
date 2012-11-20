f43baad6e3adaf99615f41c123f04093c5e36659
Ben Wilk <benjaminawilk@gmail.com>
2012-11-19 23:53:46 -0500

    total rewrite

--- DomainChecker.rb
+++ DomainChecker.rb
@@ -7,30 +7,50 @@ Version: 2.0
 
 class Domain_check
   def vhost_grab
-    return full_domain_path = `ls /etc/httpd/conf.d/vhost_* | grep -v 000_defaults.conf`.chomp.split(' ')
+     return full_domain_path = Dir["/etc/httpd/conf.d/vhost_*"]
+   #  full_domain_path.each_index do |x|
+   #    full_domain_path[x] = full_domain_path[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
+   #  end
+   #  return full_domain_path
   end
 
-  def vhost_stripper
-    prefix =  Domain_check.new.vhost_grab
-    vhost_stripped = []
-    prefix.each_index do |x|
-      vhost_stripped[x] = `echo '#{prefix[x]}' | awk -F'vhost_' '{print $2}' | awk -F'.conf' '{print $1}'`
+  def zero_remover
+    vhost =[]
+    vhosts = vhost_grab  
+    value = "000"
+    vhosts.sort!.each_index do |x|
+    if vhosts[x].include? value 
+      print ''
+    else
+       vhost[x] = vhosts[x]
+     end 
     end
-    return vhost_stripped
+  return vhost.compact
+  end
+
+  def before_after
+    stripper = zero_remover
+   stripper.each_index do |x|
+       stripper[x] = stripper[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
+    end
+    final_vhost = stripper
+   return final_vhost
   end
 
   def vhost_display
+    final_vhost = before_after
     puts "\n%s %40s %43s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
-    final_vhost = vhost_stripper
     final_vhost.each_index { |x|
       padding = 50
       print final_vhost[x].strip
       padding = padding.to_i - final_vhost[x].length
-      print "%0#{padding}s" %[`grep '<VirtualHost .*:80>' #{Domain_check.new.vhost_grab[x]} | awk -F'<VirtualHost' '{print $2}'|awk -F':' '{print $1}'`.strip.to_s]
+      print "%0#{padding}s" %[`grep '<VirtualHost .*:80>' #{vhost_grab[x]} | awk -F'<VirtualHost' '{print $2}'|awk -F':' '{print $1}'`.strip.to_s]
       puts "%40s" %[`dig #{final_vhost[x].strip} +short`]
     }
   end
 end
 
 d1 = Domain_check.new
+d1.zero_remover
 d1.vhost_display
+#d1.stripper
