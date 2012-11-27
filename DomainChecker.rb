90d447495628f948231e2471614400c51410f69c
Ben Wilk <benjaminawilk@gmail.com>
2012-11-26 23:45:38 -0500

    Finally finished proper modularity on Domainchecker

--- DomainChecker.rb
+++ DomainChecker.rb
@@ -1,59 +1,50 @@
 #!/usr/bin/env ruby
 =begin
 DomainChecker.rb: Checks to see if the domains on server are being hosted
-Last Revision: Nov 14, 2012
-Version: 2.0
+Last Revision: Nov 26, 2012
 =end
 
 class DomainCheck
   def vhost_grab
     vhosts = []
-    Dir["/etc/httpd/conf.d/vhost_*"].each { |d|
-    if d.include? "000"
-      print ""
-    else
-      vhosts.push(d)
-    end
-   }
-  return vhosts
+    Dir["/etc/httpd/conf.d/vhost_*"].each do |d|
+      if d.include? "000"
+        print ""
+      else
+        vhosts.push(d)
+      end
+    end 
+    return vhosts
   end
 
-  def stripper
-    vhosts = vhost_grab
+  def ip_address(vhosts)
     value = []
-
     vhosts.each_index do |x|
-      value[x] = open(vhosts[x]).grep(/<VirtualHost .*:80>/).to_s
-      value[x].gsub!(/:80>/,"").gsub!(/<VirtualHost/, "")
+      value[x] = open(vhosts[x]).grep(/<VirtualHost .*:80>/).to_s.gsub!(/:80>/,"").gsub!(/<VirtualHost/, "")
     end
-  return value 
+    value 
   end
 
-  def shortened
-    vhosts = vhost_grab
+  def vhost_shortener(long_vhost)
     stripped_vhosts = []
-    vhosts.each_index do |x|
-      stripped_vhosts[x] = vhosts[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
+    long_vhost.each_index do |x|
+      stripped_vhosts[x] = long_vhost[x].gsub("/etc/httpd/conf.d/vhost_", '').gsub(".conf",'')
     end
-  return stripped_vhosts
+    stripped_vhosts
   end
 
-  def domain_display
-    stripped_vhosts = shortened
-    value = stripper
-    vhosts = vhost_grab
-    puts "\n%s %40s %43s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
-    value.each_index do |x|
-
-      padding = 80
-      padding = padding.to_i - vhosts[x].length
-
-      print "%s %#{padding}s %35s" %[stripped_vhosts[x], value[x].strip, `dig #{stripped_vhosts[x].strip} +short`.strip]
+  def domain_display(final_vhost, display_ip)
+    puts "\n%s %40s %41s" %["Domain name", "IP Address Listed", "IP Address Currently in Use"]
+    display_ip.each_index do |x|
+      padding = 49
+      padding = padding.to_i - final_vhost[x].strip.length
+      print "%s %#{padding}s %35s" %[final_vhost[x], display_ip[x].strip, `dig #{final_vhost[x].strip} +short`.strip]
       puts "\n"
     end
   end
-
 end
 
 d1 = DomainCheck.new
-d1.domain_display
+display_ip = d1.ip_address(d1.vhost_grab)
+final_vhost = d1.vhost_shortener(d1.vhost_grab)
+d1.domain_display(final_vhost, display_ip)
